87b4e7af2c816e15e9e91d2c00e78cd8584395a0
Evan Drake <evanmdrake@icloud.com>
2022-04-20 20:54:27 -0700

    built db and modles to hold recipes

--- app/backend/src/models.rs
+++ app/backend/src/models.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 use crate::schema::users;
+use crate::schema::recipes;
 
+// for login
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
 pub struct User {
     pub id: String,
@@ -13,4 +15,20 @@ pub struct User {
 pub struct NewUser {
     pub user_email: String,
     pub password: String,
+}
+
+// for recipe
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
+pub struct Recipe {
+    pub id: String,
+    pub user_email: String,
+    pub prep: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NewRecipe {
+    pub user_email: String,
+    pub prep: String,
+    pub name: String,
 }
--- app/backend/src/schema.rs
+++ app/backend/src/schema.rs
@@ -1,7 +1,21 @@
 table! {
+    recipes (id) {
+        id -> Binary,
+        user_email -> Text,
+        prep -> Text,
+        name -> Text,
+    }
+}
+
+table! {
     users (id) {
         id -> Text,
         user_email -> Text,
         password -> Text,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    recipes,
+    users,
+);
